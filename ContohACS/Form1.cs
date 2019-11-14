4641c8ca0f6fcecd58a7bba3fd9a0d724efe7192
Gracielo Justine <gracielo1@mhs.stts.edu>
2019-11-14 15:56:08 +0700

    add mbox

--- ContohACS/Form1.cs
+++ ContohACS/Form1.cs
@@ -16,5 +16,10 @@ namespace ContohACS
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
